dbcbcc97cfb81204489203755994e2bc09723e57
David S <54046607+santiago-vas@users.noreply.github.com>
2021-12-23 16:10:12 -0500

    Update read_firestore_files.py

--- read_firestore_files.py
+++ read_firestore_files.py
@@ -2,10 +2,7 @@
 crdl = service_account.Credentials.from_service_account_file('../settings/storage-david-vasquez.json')
 # crdl = credentials.Certificate("../settings/storage-david-vasquez.json")
 
-
 db=firestore.Client(project='lw-looker',credentials=crdl)
-db
-
 
 result = db.collection(u'clientDatamart').document("1BGNXGQuEhEuxMJ2ZZOj").get()
 result.to_dict()
